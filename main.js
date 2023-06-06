9ba492004c33806f4c05433bcdf46907a79413a3
ibnballo <ibnballo@gmail.com>
2023-06-06 14:28:57 +0100

    Add book collections to page

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-
+const bookContainer = document.getElementById('bookshelf');
 const books = [
   {title: 'Book 1', author: 'Abdullah'},
   {title: 'Book 2', author: 'Faatimah'},
   {title: 'Book 3', author: 'Raslaan'},
 ]
+
+const ul = document.createElement('ul')
+
+books.forEach((book)=> {
+  const { title, author } = book
+  const titleList = document.createElement('li');
+  const authorList = document.createElement('li');
+  titleList.innerText = title
+  authorList.innerText = author
+  ul.append(titleList, authorList)
+})
+
+bookContainer.appendChild(ul)
