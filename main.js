aa61167398699ef75b14020de9e15d3c11f8e76b
ibnballo <ibnballo@gmail.com>
2023-06-09 09:34:31 +0100

    Update file

--- main.js
+++ main.js
@@ -2,23 +2,39 @@ const bookContainer = document.getElementById('bookshelf');
 const ul = document.createElement('ul');
 const form = document.getElementById('form');
 const addBtn = document.getElementById('add-btn');
-const titleInput = document.getElementById('book-title').value;
-const authorInput = document.getElementById('book-author').value;
+const titleInput = document.getElementById('book-title');
+const authorInput = document.getElementById('book-author');
 const books = [
-  {title: 'Book 1', author: 'Abdullah'},
-  {title: 'Book 2', author: 'Faatimah'},
-  {title: 'Book 3', author: 'Raslaan'},
-]
+  { title: 'Book 1', author: 'Abdullah' },
+  { title: 'Book 2', author: 'Faatimah' },
+  { title: 'Book 3', author: 'Raslaan' },
+];
 
-books.forEach((book)=> {
-  const { title, author } = book
+books.forEach((book) => {
+  const { title, author } = book;
   const titleList = document.createElement('li');
   const authorList = document.createElement('li');
-  titleList.innerText = title
-  authorList.innerText = author
-  ul.append(titleList, authorList)
-})
+  titleList.innerText = title;
+  authorList.innerText = author;
+  ul.append(titleList, authorList);
+});
 
 // Function to add book to collection
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // Prevent the default form submission behavior
 
-bookContainer.appendChild(ul)
+  const title = titleInput.value;
+  const author = authorInput.value;
+
+  console.log('This is the event:', e);
+  console.log('Title:', title);
+  console.log('Author:', author);
+
+  // Additional actions after logging the event and values
+
+  // Clear input fields
+  titleInput.value = '';
+  authorInput.value = '';
+});
+
+bookContainer.appendChild(ul);
